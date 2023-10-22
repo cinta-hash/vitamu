dcfcb529912d0abf8c875abbc0f2da1a7e7b64ad
jacinta mwangi <jacinta.mwangi.student@moringaschool.com>
2023-10-22 11:29:41 +0300

    added db

---
+++ src/data.jsx
@@ -0,0 +1,86 @@
+export default menu
+    
+    const menu= [
+      {
+        "id": 1,
+        "name": "Classic Burger",
+        "description": "A juicy beef patty with fresh lettuce, tomatoes, and special sauce.",
+        "imageURL": "https://example.com/classic_burger.jpg",
+        "rating": 4.5,
+        "discountApplies": true
+      },
+      {
+        "id": 2,
+        "name": "Margherita Pizza",
+        "description": "Traditional pizza with tomato sauce, fresh mozzarella, and basil.",
+        "imageURL": "https://example.com/margherita_pizza.jpg",
+        "rating": 4.2,
+        "discountApplies": false
+      },
+      {
+        "id": 3,
+        "name": "Chicken Alfredo Pasta",
+        "description": "Creamy Alfredo sauce with grilled chicken over fettuccine pasta.",
+        "imageURL": "https://example.com/chicken_alfredo_pasta.jpg",
+        "rating": 4.8,
+        "discountApplies": true
+      },
+      {
+        "id": 4,
+        "name": "Vegetarian Sushi Roll",
+        "description": "Avocado, cucumber, and carrot rolled in seasoned rice and seaweed.",
+        "imageURL": "https://example.com/vegetarian_sushi_roll.jpg",
+        "rating": 4.0,
+        "discountApplies": false
+      },
+      {
+        "id": 5,
+        "name": "Grilled Salmon",
+        "description": "Freshly grilled salmon fillet with lemon and herbs.",
+        "imageURL": "https://example.com/grilled_salmon.jpg",
+        "rating": 4.7,
+        "discountApplies": false
+      },
+      {
+        "id": 6,
+        "name": "Caesar Salad",
+        "description": "Crisp romaine lettuce with Caesar dressing, croutons, and Parmesan cheese.",
+        "imageURL": "https://example.com/caesar_salad.jpg",
+        "rating": 4.3,
+        "discountApplies": true
+      },
+      {
+        "id": 7,
+        "name": "Mango Tango Smoothie",
+        "description": "Refreshing smoothie with mango, banana, and yogurt.",
+        "imageURL": "https://example.com/mango_tango_smoothie.jpg",
+        "rating": 4.6,
+        "discountApplies": false
+      },
+      {
+        "id": 8,
+        "name": "BBQ Chicken Wings",
+        "description": "Crispy chicken wings glazed with barbecue sauce.",
+        "imageURL": "https://example.com/bbq_chicken_wings.jpg",
+        "rating": 4.4,
+        "discountApplies": true
+      },
+      {
+        "id": 9,
+        "name": "Chocolate Lava Cake",
+        "description": "Warm chocolate cake with a gooey, molten center.",
+        "imageURL": "https://example.com/chocolate_lava_cake.jpg",
+        "rating": 4.9,
+        "discountApplies": false
+      },
+      {
+        "id": 10,
+        "name": "Caprese Panini",
+        "description": "Grilled panini with mozzarella, tomatoes, and basil.",
+        "imageURL": "https://example.com/caprese_panini.jpg",
+        "rating": 4.1,
+        "discountApplies": true
+      }
+    ]
+  }
+  
