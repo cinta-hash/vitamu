acf3f243e9eb7e89c70f90a92a2b5d7b61932bd2
jacinta mwangi <jacintawmwangi21@gmail.com>
2023-11-08 11:39:40 +0300

    added mobile responsiveness

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,22 +7,24 @@ export default function Header(){
             <img className="w-12 md:w-20 m-auto rounded-full" src="/logo.jpeg" alt="vitamu-logo" />
             <h1 className="text-4xl font-serif font-extrabold p-2">vitamu</h1>
           </div>
-          <h1 className="text-xl md:text-3xl text-orange-600 p-2 text-center md:space-x-4"><i class="fa-solid fa-phone"><span className="hidden md:inline text-lg p-2">call to order</span></i></h1>
-          <div className="flex">
+          <div className="pt-2"> 
+            <h1 className="text-xl md:text-3xl text-orange-600 p-2 text-center md:space-x-4"><i class="fa-solid fa-phone"><span className="hidden md:inline text-lg p-2">call to order</span></i></h1>
+            </div>
+          <div className="flex pt-2">
              <h1 className="md:text-3xl text-xl text-orange-600 p-2 text-center font-bold"><i class="fa-solid fa-magnifying-glass"></i></h1>
              <div className="m-2 hidden md:inline-flex md:relative">
                 <input type="text" id="search" placeholder="search food" className="rounded-md shadow-md shadow-slate-300 p-2 w-full"/>
                 <button className="absolute right-2 top-1/2 transform -translate-y-1/2"><i class="fa-solid fa-xmark"></i></button>
              </div>
           </div>
-          <div className="flex">
+          <div className="flex pt-2">
             <h1 className="md:text-3xl text-xl text-orange-600 p-2 text-center font-bold md:space-x-4"><i class="fa-solid fa-location-dot"></i></h1>
             <div className="hidden md:inline-flex md:relative md:m-2">
                 <input type="text" id="search-loc" placeholder="choose location" className="rounded-md shadow-md shadow-slate-300 p-2 w-full"/>
                 <button className="absolute right-2 top-1/2 transform -translate-y-1/2"><i class="fa-solid fa-xmark"></i></button>
              </div>
           </div>
-          <div className="flex">
+          <div className="flex pt-2">
             <h1 className="dark md:text-3xl text-xl text-black p-2 font-bold space-x-4 cursor-pointer"><i class="fa-solid fa-moon fa-flip-horizontal"></i></h1>
             <h1 className="light md:text-3xl text-xl text-black p-2 font-bold cursor-pointer"><i class="fa-solid fa-sun"></i></h1>    
           </div>     
